2f16fabd154a9305ce7fddace8a15fc552a4deb3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-29 16:28:27 +0200

    added credits to EssentialsX

--- src/main/java/de/jeff_media/jefflib/ReflUtil.java
+++ src/main/java/de/jeff_media/jefflib/ReflUtil.java
@@ -15,6 +15,10 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Proudly "stolen" from EssentialsX
+ * https://github.com/EssentialsX/Essentials
+ */
 public final class ReflUtil {
     public static final NMSVersion V1_12_R1 = NMSVersion.fromString("v1_12_R1");
     public static final NMSVersion V1_9_R1 = NMSVersion.fromString("v1_9_R1");
