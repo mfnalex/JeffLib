f7867a5931e66b0965d7fe38bfaa58a025def506
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-08-12 14:23:22 +0200

    added hex colors and emojis

--- src/main/java/de/jeff_media/jefflib/TextUtils.java
+++ src/main/java/de/jeff_media/jefflib/TextUtils.java
@@ -1,9 +1,17 @@
 package de.jeff_media.jefflib;
 
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * Provides some methods to print nice formatted texts/banners to console
+ * Provides some text related methods
  */
 public class TextUtils {
 
@@ -11,14 +19,70 @@ public class TextUtils {
     private static final char BANNER_CHAR = '#';
     private static final char SPACE = ' ';
     private static final String EMPTY = "";
+    private static final String AMPERSAND_HASH_REGEX = "&#([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])";
+    private static final Pattern AMPERSAND_HASH_PATTERN = Pattern.compile(AMPERSAND_HASH_REGEX);
+    private static final String XML_LIKE_HASH_REGEX = "<#([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])>";
+    private static final Pattern XML_LIKE_HASH_PATTERN = Pattern.compile(XML_LIKE_HASH_REGEX);
+    private static AtomicReference<Plugin> itemsAdderPlugin = null;
 
     /**
      * Prints a banner / headline to console
+     *
      * @param text text to be printed
      */
     public static void banner(String text) {
-        final int bannerWidth = Math.max(text.length()+4,MIN_BANNER_WIDTH);
-        StringUtils.leftPad(EMPTY,bannerWidth,BANNER_CHAR);
-        System.out.println(StringUtils.center(SPACE + text + SPACE,bannerWidth, BANNER_CHAR));
+        final int bannerWidth = Math.max(text.length() + 4, MIN_BANNER_WIDTH);
+        StringUtils.leftPad(EMPTY, bannerWidth, BANNER_CHAR);
+        System.out.println(StringUtils.center(SPACE + text + SPACE, bannerWidth, BANNER_CHAR));
+    }
+
+    private static String addAmpersandsToHex(String hex) {
+        if (hex.length() != 6) {
+            throw new IllegalArgumentException("Hex-Codes must always have 6 letters.");
+        }
+        char[] chars = hex.toCharArray();
+        StringBuilder sb = new StringBuilder("&x");
+        for (char aChar : chars) {
+            sb.append("&").append(aChar);
+        }
+        return sb.toString();
+    }
+
+    private static String replaceRegexWithGroup(String text, Pattern pattern, int group, Function<String, String> function) {
+        Matcher matcher = pattern.matcher(text);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, function.apply(matcher.group(group)));
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
+    }
+
+    public static String replaceEmojis(String text) {
+        if (itemsAdderPlugin == null) {
+            itemsAdderPlugin = new AtomicReference<>(Bukkit.getPluginManager().getPlugin("ItemsAdder"));
+        }
+        if(itemsAdderPlugin.get()!=null) {
+            text = dev.lone.itemsadder.api.FontImages.FontImageWrapper.replaceFontImages(text);
+        }
+        return text;
+    }
+
+    /**
+     * Replaces color codes using &. Also supports hex colors using &x&r&r&g&g&b&b, &#rrggbb and <#rrggbb>
+     *
+     * @param text
+     * @return
+     */
+    public static String color(String text) {
+        text = replaceRegexWithGroup(text, AMPERSAND_HASH_PATTERN, 1, TextUtils::addAmpersandsToHex);
+        text = replaceRegexWithGroup(text, XML_LIKE_HASH_PATTERN, 1, TextUtils::addAmpersandsToHex);
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    public static String format(String text) {
+        text = color(text);
+        text = replaceEmojis(text);
+        return text;
     }
 }
