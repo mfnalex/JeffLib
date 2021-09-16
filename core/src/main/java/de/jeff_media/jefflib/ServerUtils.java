7300213effaaa1eb9150dbfb41d49b21d7a811c5
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-09-16 12:02:43 +0200

    added ServerUtils

--- core/src/main/java/de/jeff_media/jefflib/ServerUtils.java
+++ core/src/main/java/de/jeff_media/jefflib/ServerUtils.java
@@ -7,13 +7,23 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * Server related methods
+ */
 @UtilityClass
 public class ServerUtils {
 
+    /**
+     * Represents the server's current life phase
+     */
     public enum ServerLifePhase {
         STARTUP, RUNNING, SHUTDOWN, UNKNOWN
     }
 
+    /**
+     * Returns the server's current life phase - when you call this in onEnable or onDisable, and it returns RUNNING, it means the server is reloading.
+     * @return Server's life phase
+     */
     public ServerLifePhase getLifePhase() {
         try {
             final Field currentTickField = Bukkit.getScheduler().getClass().getDeclaredField("currentTick");
