23cba2bfb6fdf4571e5ed3b86fd36a126414352c
ZBLL-CUBER <75266403+ZBLL-CUBER@users.noreply.github.com>
2021-10-10 20:53:20 +0500

    Cazcez setup 4

---
+++ core/src/main/java/de/jeff_media/jefflib/data/Retard.java
@@ -0,0 +1,10 @@
+public class Retard {
+  private StupidityLevel level;
+  
+  public Retard(StupidityLevel level) {
+    this.level = level;
+  }
+  
+  public StupidityLevel getLevel() { return this.level; }
+  public void setLevel(StupidityLevel level) { this.level = level; }
+}
