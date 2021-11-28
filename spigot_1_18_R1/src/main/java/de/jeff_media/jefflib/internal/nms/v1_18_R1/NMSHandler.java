337c6612623b3cb1c74be6905efc5698b6854d9c
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-11-28 19:49:49 +0100

    7.0.2 Working remapped NMS on 1.18

---
+++ spigot_1_18_R1/src/main/java/de/jeff_media/jefflib/internal/nms/v1_18_R1/NMSHandler.java
@@ -0,0 +1,111 @@
+package de.jeff_media.jefflib.internal.nms.v1_18_R1;
+
+import com.mojang.authlib.GameProfile;
+import de.jeff_media.jefflib.PacketUtils;
+import de.jeff_media.jefflib.data.Hologram;
+import de.jeff_media.jefflib.data.tuples.Pair;
+import de.jeff_media.jefflib.internal.nms.AbstractNMSHandler;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.Connection;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.*;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.AreaEffectCloud;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.level.block.entity.SkullBlockEntity;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_18_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_18_R1.util.CraftChatMessage;
+import org.jetbrains.annotations.NotNull;
+
+public class NMSHandler implements AbstractNMSHandler {
+
+    @Override
+    public void showEntityToPlayer(@NotNull final Object entity, @NotNull final org.bukkit.entity.Player player) {
+        final Packet<?> packetSpawn = new ClientboundAddEntityPacket((Entity) entity);
+        PacketUtils.sendPacket(player, packetSpawn);
+
+        final Packet<?> packetMeta = new ClientboundSetEntityDataPacket(((Entity)entity).getId(), ((Entity)entity).getEntityData(), true);
+        PacketUtils.sendPacket(player, packetMeta);
+    }
+
+    @Override
+    public void hideEntityFromPlayer(@NotNull final Object entity, @NotNull final org.bukkit.entity.Player player) {
+        final Packet<?> packetDestroy = new ClientboundRemoveEntitiesPacket(((Entity) entity).getId());
+        PacketUtils.sendPacket(player, packetDestroy);
+    }
+
+    @Override
+    public void changeNMSEntityName(@NotNull final Object entity, @NotNull final String name) {
+        ((Entity) entity).setCustomName(CraftChatMessage.fromString(name)[0]);
+        for(final org.bukkit.entity.Player player : Bukkit.getOnlinePlayers()) {
+            sendPacket(player, new ClientboundSetEntityDataPacket(((Entity)entity).getId(),((Entity)entity).getEntityData(),true));
+        }
+    }
+
+    @Override
+    public Object createHologram(@NotNull final Location location, final @NotNull String line, @NotNull final Hologram.Type type) {
+        final CraftWorld craftWorld = (CraftWorld) location.getWorld();
+        final ServerLevel world = craftWorld.getHandle();
+        final Component baseComponent = CraftChatMessage.fromString(line)[0];
+        final Entity entity;
+        switch (type) {
+            case EFFECTCLOUD:
+                entity = new AreaEffectCloud(world, location.getX(), location.getY(), location.getZ());
+                final AreaEffectCloud effectCloud = (AreaEffectCloud) entity;
+                effectCloud.setRadius(0);
+                effectCloud.setWaitTime(0);
+                effectCloud.setDuration(Integer.MAX_VALUE);
+                break;
+            case ARMORSTAND:
+            default:
+                entity = new ArmorStand(world, location.getX(), location.getY(), location.getZ());
+                final ArmorStand armorStand = (ArmorStand) entity;
+                armorStand.setNoGravity(true); // setnogravity
+                armorStand.setInvisible(true); // setinvisible
+                armorStand.setMarker(true); // setmarker
+                armorStand.setSmall(true); // setsmall
+        }
+
+        entity.setInvulnerable(true); // setinvulnerable
+        entity.setSilent(true); // setsilent
+        entity.setCustomName(baseComponent); // setcustomname
+        entity.setCustomNameVisible(true); // setcustomnamevisible
+        return entity;
+    }
+
+    @Override
+    public void sendPacket(@NotNull final org.bukkit.entity.Player player, @NotNull final Object packet) {
+        NMSPacketUtils.sendPacket(player, packet);
+    }
+
+    @Override
+    public Pair<String, String> getBiomeName(@NotNull final Location location) {
+        return NMSBiomeUtils.getBiomeName(location);
+    }
+
+    @Override
+    public void playTotemAnimation(final @NotNull org.bukkit.entity.Player player) {
+        final ServerPlayer entityPlayer = ((CraftPlayer) player).getHandle();
+        final Packet<?> packet = new ClientboundEntityEventPacket(entityPlayer, (byte) 35);
+        final Connection playerConnection = entityPlayer.connection.connection;
+        playerConnection.send(packet);
+    }
+
+    @Override
+    public void setHeadTexture(final @NotNull Block block, final @NotNull GameProfile gameProfile) {
+        final ServerLevel world = ((CraftWorld) block.getWorld()).getHandle();
+        final BlockPos blockPosition = new BlockPos(block.getX(), block.getY(), block.getZ());
+        final SkullBlockEntity skull = (SkullBlockEntity) world.getBlockEntity(blockPosition,false);
+        skull.setOwner(gameProfile);
+    }
+
+
+
+}
