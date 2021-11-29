e0f7216ed92fca81ae02cbb45f314905cbaed978
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-11-29 13:53:18 +0100

    7.0.4 Added InventoryUtils#addOrDrop

--- core/src/main/java/de/jeff_media/jefflib/InventoryUtils.java
+++ core/src/main/java/de/jeff_media/jefflib/InventoryUtils.java
@@ -1,14 +1,18 @@
 package de.jeff_media.jefflib;
 
 import lombok.experimental.UtilityClass;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 
 /**
  * Inventory related methods
@@ -59,6 +63,53 @@ public final class InventoryUtils {
         inventory.all(material).values()
                 .forEach(itemStack -> amount.addAndGet(itemStack.getAmount()));
         return amount.get();
+    }
+
+    /**
+     * Adds all the given ItemStacks to the player's inventory, or drops them at the given location
+     * @param player Player
+     * @param dropLocation Location where to drop leftover items
+     * @param items ItemStacks to add
+     * @return true when all items could be stored, otherwise false
+     */
+    public static boolean addOrDrop(final @NotNull Player player, final @Nullable Location dropLocation, final @NotNull Iterable<ItemStack> items) {
+        boolean storedEverything = true;
+        for(final ItemStack item : items) {
+            player.getInventory().addItem(item).forEach((slot, leftover) -> dropLocation.getWorld().dropItemNaturally(dropLocation, leftover));
+            storedEverything = false;
+        }
+        return storedEverything;
+    }
+
+    /**
+     * Adds all the given ItemStacks to the player's inventory, or drops them at the player's location
+     * @param player Player
+     * @param items ItemStacks to add
+     * @return true when all items could be stored, otherwise false
+     */
+    public static boolean addOrDrop(final @NotNull Player player, final @NotNull Iterable<ItemStack> items) {
+        return addOrDrop(player, player.getLocation(), items);
+    }
+
+    /**
+     * Adds all the given ItemStacks to the player's inventory, or drops them at the given location
+     * @param player Player
+     * @param dropLocation Location where to drop leftover items
+     * @param items ItemStacks to add
+     * @return true when all items could be stored, otherwise false
+     */
+    public static boolean addOrDrop(final @NotNull Player player, final @Nullable Location dropLocation, final @NotNull ItemStack... items) {
+        return addOrDrop(player, dropLocation, Arrays.asList(items));
+    }
+
+    /**
+     * Adds all the given ItemStacks to the player's inventory, or drops them at the player's location
+     * @param player Player
+     * @param items ItemStacks to add
+     * @return true when all items could be stored, otherwise false
+     */
+    public static boolean addOrDrop(final @NotNull Player player, final @NotNull ItemStack... items) {
+        return addOrDrop(player, player.getLocation(), Arrays.asList(items));
     }
 
 }
