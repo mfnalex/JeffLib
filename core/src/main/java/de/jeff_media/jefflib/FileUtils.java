45a26021a43f9a9ac39113698aa2d590ff292291
mfnalex <1122571+mfnalex@users.noreply.github.com>
2022-04-02 09:05:35 +0200

    Propagate exceptions, use try-with-resources, add JeffLibNotInitializedException#check()

--- core/src/main/java/de/jeff_media/jefflib/FileUtils.java
+++ core/src/main/java/de/jeff_media/jefflib/FileUtils.java
@@ -1,15 +1,16 @@
 package de.jeff_media.jefflib;
 
 
+import de.jeff_media.jefflib.exceptions.JeffLibNotInitializedException;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.bukkit.plugin.Plugin;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -24,15 +25,11 @@ public final class FileUtils {
      * @param file  File to write to
      * @param lines lines to write
      */
-    public static void writeToFile(final File file, final Iterable<String> lines) {
-        try {
-            final Writer output = new OutputStreamWriter(new FileOutputStream(file, false), StandardCharsets.UTF_8);
+    public static void writeToFile(final File file, final Iterable<String> lines) throws IOException {
+        try (final Writer output = new OutputStreamWriter(new FileOutputStream(file, false), StandardCharsets.UTF_8);) {
             for (final String line : lines) {
                 output.append(line).append(System.lineSeparator());
             }
-            output.close();
-        } catch (final IOException ioException) {
-            ioException.printStackTrace();
         }
     }
 
@@ -42,7 +39,7 @@ public final class FileUtils {
      * @param file File to append to
      * @param line line to append
      */
-    public static void appendLines(final File file, final String line) {
+    public static void appendLines(final File file, final String line) throws IOException {
         appendLines(file, new String[]{line});
     }
 
@@ -52,15 +49,11 @@ public final class FileUtils {
      * @param file  File to append to
      * @param lines lines to append
      */
-    public static void appendLines(final File file, final String[] lines) {
-        try {
-            final Writer output = new OutputStreamWriter(new FileOutputStream(file, true), StandardCharsets.UTF_8);
+    public static void appendLines(final File file, final String[] lines) throws IOException {
+        try (final Writer output = new OutputStreamWriter(new FileOutputStream(file, true), StandardCharsets.UTF_8);) {
             for (final String line : lines) {
                 output.append(line).append(System.lineSeparator());
             }
-            output.close();
-        } catch (final IOException ioException) {
-            ioException.printStackTrace();
         }
     }
 
@@ -70,7 +63,7 @@ public final class FileUtils {
      * @param file  File to append to
      * @param lines lines to append
      */
-    public static void appendLines(final File file, final Collection<String> lines) {
+    public static void appendLines(final File file, final Collection<String> lines) throws IOException {
         appendLines(file, lines.toArray(new String[0]));
     }
 
@@ -83,8 +76,8 @@ public final class FileUtils {
      */
     @SneakyThrows
     public static List<String> readFileFromResources(final Plugin plugin, final String fileName) {
-        try(InputStream input = plugin.getResource(fileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+        try (final InputStream input = plugin.getResource(fileName);
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(input)))) {
             return reader.lines().collect(Collectors.toList());
         }
     }
@@ -98,27 +91,23 @@ public final class FileUtils {
      * @return true if the file has been changed, otherwise false
      */
     public static boolean replaceStringsInFile(final File file, final CharSequence toSearch, final CharSequence replace) throws IOException {
-
         boolean changed = false;
-        final BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-        final StringBuilder inputBuffer = new StringBuilder();
-        String line;
-
-        while ((line = bufferedReader.readLine()) != null) {
-            if (line.contains(toSearch)) {
-                line = line.replace(toSearch, replace);
-                changed = true;
+        try (final BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            final StringBuilder inputBuffer = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains(toSearch)) {
+                    line = line.replace(toSearch, replace);
+                    changed = true;
+                }
+                inputBuffer.append(line);
+                inputBuffer.append(System.lineSeparator());
             }
-            inputBuffer.append(line);
-            inputBuffer.append(System.lineSeparator());
-        }
-        bufferedReader.close();
-
-        if (changed) {
-            // write the new string with the replaced line OVER the same file
-            final FileOutputStream fileOut = new FileOutputStream(file);
-            fileOut.write(inputBuffer.toString().getBytes());
-            fileOut.close();
+            if (changed) {
+                try (final FileOutputStream fileOut = new FileOutputStream(file);) {
+                    fileOut.write(inputBuffer.toString().getBytes());
+                }
+            }
         }
         return changed;
     }
@@ -130,6 +119,7 @@ public final class FileUtils {
      * @return the File
      */
     public static File getFile(final String... path) {
+        JeffLibNotInitializedException.check();
         File file = JeffLib.getPlugin().getDataFolder();
         for (final String s : path) {
             file = new File(file, s);
