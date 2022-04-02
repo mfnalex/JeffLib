fa3f73de0239801708ad63de4dca026baf78d1a2
mfnalex <1122571+mfnalex@users.noreply.github.com>
2022-04-02 09:05:47 +0200

    formatting

--- core/src/main/java/de/jeff_media/jefflib/HologramManager.java
+++ core/src/main/java/de/jeff_media/jefflib/HologramManager.java
@@ -1,6 +1,5 @@
 package de.jeff_media.jefflib;
 
-import de.jeff_media.jefflib.JeffLib;
 import de.jeff_media.jefflib.data.Hologram;
 import de.jeff_media.jefflib.exceptions.JeffLibNotInitializedException;
 import de.jeff_media.jefflib.exceptions.NMSNotSupportedException;
@@ -22,37 +21,29 @@ public class HologramManager {
     @Getter
     private static final List<Hologram> holograms = new ArrayList<>();
     private static final Map<OfflinePlayer, List<Hologram>> SHOWN_HOLOGRAMS = new HashMap<>();
-    private static boolean IS_SCHEDULED = false;
-
     private static final Runnable RUNNABLE = () -> {
-        for(final Player player : Bukkit.getOnlinePlayers()) {
-            for(final Hologram hologram : holograms) {
-                if(!SHOWN_HOLOGRAMS.containsKey(player)) {
-                    SHOWN_HOLOGRAMS.put(player,new ArrayList<>());
+        for (final Player player : Bukkit.getOnlinePlayers()) {
+            for (final Hologram hologram : holograms) {
+                if (!SHOWN_HOLOGRAMS.containsKey(player)) {
+                    SHOWN_HOLOGRAMS.put(player, new ArrayList<>());
                 }
 
                 // Show hologram
-                if(
-                        !SHOWN_HOLOGRAMS.get(player).contains(hologram)
-                        && (hologram.isVisibleForAnyone() || hologram.getPlayers().contains(player))
-                        && hologram.getLocation().getWorld().equals(player.getWorld())
-                        && hologram.getLocation().distanceSquared(player.getLocation()) <= (hologram.getVisibilityRadius()*hologram.getVisibilityRadius())) {
+                if (!SHOWN_HOLOGRAMS.get(player).contains(hologram) && (hologram.isVisibleForAnyone() || hologram.getPlayers().contains(player)) && hologram.getLocation().getWorld().equals(player.getWorld()) && hologram.getLocation().distanceSquared(player.getLocation()) <= (hologram.getVisibilityRadius() * hologram.getVisibilityRadius())) {
                     SHOWN_HOLOGRAMS.get(player).add(hologram);
-                    for(final Object entity : hologram.getEntities()) {
+                    for (final Object entity : hologram.getEntities()) {
                         JeffLib.getNMSHandler().showEntityToPlayer(entity, player);
                     }
                 } else {
 
                     // Hide hologram
-                    if (!SHOWN_HOLOGRAMS.get(player).contains(hologram))       {
+                    if (!SHOWN_HOLOGRAMS.get(player).contains(hologram)) {
                         continue;
                     }
 
-                    if(
-                            !hologram.getLocation().getWorld().equals(player.getWorld())
-                            || hologram.getLocation().distanceSquared(player.getLocation()) > (hologram.getVisibilityRadius()*hologram.getVisibilityRadius())) {
+                    if (!hologram.getLocation().getWorld().equals(player.getWorld()) || hologram.getLocation().distanceSquared(player.getLocation()) > (hologram.getVisibilityRadius() * hologram.getVisibilityRadius())) {
                         SHOWN_HOLOGRAMS.get(player).remove(hologram);
-                        for(final Object entity : hologram.getEntities()) {
+                        for (final Object entity : hologram.getEntities()) {
                             JeffLib.getNMSHandler().hideEntityFromPlayer(entity, player);
                         }
                     }
@@ -60,14 +51,15 @@ public class HologramManager {
             }
         }
     };
+    private static boolean IS_SCHEDULED = false;
 
     public static void unloadAllHolograms() {
-        for(final Hologram hologram : holograms) {
-            for(final Player player : Bukkit.getOnlinePlayers()) {
+        for (final Hologram hologram : holograms) {
+            for (final Player player : Bukkit.getOnlinePlayers()) {
                 if (SHOWN_HOLOGRAMS.containsKey(player)) {
-                    if(SHOWN_HOLOGRAMS.get(player).contains(hologram)) {
+                    if (SHOWN_HOLOGRAMS.get(player).contains(hologram)) {
                         SHOWN_HOLOGRAMS.get(player).remove(hologram);
-                        for(final Object entity : hologram.getEntities()) {
+                        for (final Object entity : hologram.getEntities()) {
                             JeffLib.getNMSHandler().hideEntityFromPlayer(entity, player);
                         }
                     }
@@ -85,14 +77,14 @@ public class HologramManager {
 
     public static void init() {
 
-        if(JeffLib.getPlugin() == null) {
+        if (JeffLib.getPlugin() == null) {
             throw new JeffLibNotInitializedException();
         }
         NMSNotSupportedException.check();
 
-        if(!IS_SCHEDULED) {
+        if (!IS_SCHEDULED) {
             IS_SCHEDULED = true;
-            Bukkit.getScheduler().runTaskTimer(JeffLib.getPlugin(),RUNNABLE,5,5);
+            Bukkit.getScheduler().runTaskTimer(JeffLib.getPlugin(), RUNNABLE, 5, 5);
         }
     }
 
