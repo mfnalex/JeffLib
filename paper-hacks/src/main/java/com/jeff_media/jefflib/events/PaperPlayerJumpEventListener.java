6b6b44d12a0f2865d83c01f05499293d90be91b9
mfnalex <mfnalex@jeff-media.com>
2024-06-03 19:59:58 +0200

    14.0.8

--- paper-hacks/src/main/java/com/jeff_media/jefflib/events/PaperPlayerJumpEventListener.java
+++ paper-hacks/src/main/java/com/jeff_media/jefflib/events/PaperPlayerJumpEventListener.java
@@ -18,17 +18,40 @@
 package com.jeff_media.jefflib.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.lang.reflect.Constructor;
+
 public class PaperPlayerJumpEventListener implements Listener {
+
+    private static final Class<?> JUMP_EVENT_CLASS;
+    private static final Constructor<?> JUMP_EVENT_CONSTRUCTOR;
+
+    static {
+        try {
+            JUMP_EVENT_CLASS = Class.forName("com.jeff_media.jefflib.events.PlayerJumpEvent");
+            JUMP_EVENT_CONSTRUCTOR = JUMP_EVENT_CLASS.getConstructor(Player.class, Location.class, Location.class);
+        } catch (ReflectiveOperationException ex) {
+            throw new RuntimeException("Could not get PlayerJumpEvent constructor");
+        }
+    }
 
     @EventHandler
     public void onJump(com.destroystokyo.paper.event.player.PlayerJumpEvent paperEvent) {
-        PlayerJumpEvent ownEvent = new PlayerJumpEvent(paperEvent.getPlayer(), paperEvent.getFrom(), paperEvent.getTo());
-        Bukkit.getPluginManager().callEvent(ownEvent);
-        if (ownEvent.isCancelled()) {
-            paperEvent.setCancelled(true);
+        try {
+            Event ownEvent = (Event) JUMP_EVENT_CONSTRUCTOR.newInstance(paperEvent.getPlayer(), paperEvent.getFrom(), paperEvent.getTo());
+            Cancellable ownEventCancellable = (Cancellable) ownEvent;
+            Bukkit.getPluginManager().callEvent(ownEvent);
+            if (ownEventCancellable.isCancelled()) {
+                paperEvent.setCancelled(true);
+            }
+        } catch (ReflectiveOperationException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
